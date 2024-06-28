122399a44510bbeb6b2ae2e40dd21409b91384fb
zeb33n <zeb.bond@33n.co.uk>
2024-06-28 14:50:33 +0100

    added pipe client

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from collections.abc import Callable
+from pipe_client import PipeClient
 import random
 import time
 import os
@@ -21,9 +22,16 @@ class GameState:
     bots: dict[str, int]
     num_dice: int
     round_score: int
+    roll: list[int]
+    turn: str
 
-    def to_json(self) -> bytes:
-        return bytes(json.dumps(asdict(self)).encode("utf-8"))
+    def to_bot(self) -> bytes:
+        bot_info = ("bots", "num_dice", "round_score")
+        out_json = {k: v for k, v in asdict(self).items() if k in bot_info}
+        return bytes(json.dumps(out_json).encode("utf-8"))
+
+    def to_tui(self) -> str:
+        return json.dumps(asdict(self))
 
 
 def make_bot(name: str, extension: str) -> Callable[[bytes], bool]:
@@ -54,48 +62,29 @@ def make_bot(name: str, extension: str) -> Callable[[bytes], bool]:
 class App:
     def __init__(self):
         self.bots = self.load_bots()
-        self.game_state = GameState({name: 0 for name in self.bots}, 6, 0)
-        self.initialise_screen()
-        self.update_scores()
+        self.pipe_client = PipeClient()
+        self.game_state = GameState({name: 0 for name in self.bots}, 6, 0, [], "")
 
     def load_bots(self) -> dict[str, Callable[[bytes], bool]]:
         bot_info = tuple(botfile.rsplit(".", 1) for botfile in os.listdir(BOT_DIR_LOC))
         return {name: make_bot(name, extension) for name, extension in bot_info}
 
-    def update_scores(self):
-        magic = "\033[F"
-        update = "\n".join(
-            [f"{name}: {score}" for name, score in self.game_state.bots.items()]
-        )
-        magic = magic * (update.count("\n") + (LINES + 1))
-        print(f"{magic}{update}{'\n' * LINES}")
-
-    def initialise_screen(self):
-        print("SCORES")
-        for _ in self.game_state.bots:
-            print()
-        for _ in range(LINES):
-            print()
-
     def roll_dice(self):
         return [random.randint(1, 6) for _ in range(self.game_state.num_dice)]
 
     def main(self):
-        while True:
+        while max(self.game_state.bots.values()) < 10000:
             for bot_name, run_bot in self.bots.items():
                 self.game_state.round_score = 0
-                while run_bot(self.game_state.to_json()):
+                while run_bot(self.game_state.to_bot()):
                     self.game_state.round_score += 100
-                    print("\033[F" * 4)
-                    print((" " * 50 + "\n") * 2)
-                    print("\033[F" * 4)
-                    print(f"{bot_name}'s turn", flush=True)
-                    print(f"current score: {self.game_state.round_score}")
-                    print(f"rolled {self.roll_dice()}")
+                    self.game_state.turn = bot_name
+                    self.game_state.roll = self.roll_dice()
+                    self.pipe_client.pipe(self.game_state.to_tui())
                     time.sleep(1)
                 else:
                     self.game_state.bots[bot_name] += self.game_state.round_score
-                    self.update_scores()
+        self.pipe_client.cleanup()
 
 
 if __name__ == "__main__":
