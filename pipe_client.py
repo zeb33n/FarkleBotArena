88132fc24347f8d232feb4d73dd52b1842c762b6
william <william-cullum@hotmail.co.uk>
2024-06-29 10:12:48 +0100

    added conditional in pipe client to avoid trying to create an already existing client

--- pipe_client.py
+++ pipe_client.py
@@ -4,7 +4,8 @@ import os
 class PipeClient:
     def __init__(self, pipename: str = "pipe") -> None:
         self._pipename = pipename
-        os.mkfifo(self._pipename)
+        if not os.path.exists(self._pipename):
+            os.mkfifo(self._pipename)
 
     def pipe(self, msg: str):
         with open(self._pipename, "w") as p:
