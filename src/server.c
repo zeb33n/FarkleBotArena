35f9b1c2695e8d5cd19e0f2990cdbddeba1b9445
zeb33n <zeb.bond@33n.co.uk>
2024-06-30 20:50:21 +0100

    refactor

--- src/server.c
+++ src/server.c
@@ -5,23 +5,53 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-char *read_pipe(int fd) {
-  static char out[256] = {0};
-  int bytes_read = read(fd, out, 255);
+// char *read_pipe(int fd) {
+//   static char out[256] = {0};
+//   int bytes_read = read(fd, out, 255);
+//   if (bytes_read < 0) {
+//     perror("read");
+//     return NULL;
+//   }
+//   out[bytes_read] = '\0';
+//   return out;
+// }
+
+int send_game_state(int pipefd, int clientfd) {
+  char game_state[256] = {0};
+  int bytes_read = read(pipefd, game_state, 255);
   if (bytes_read < 0) {
     perror("read");
-    return NULL;
+    return -1;
   }
-  out[bytes_read] = '\0';
-  return out;
+  game_state[bytes_read] = '\0';
+
+  long send_err = send(clientfd, game_state, 255, 0);
+  if (send_err == -1) {
+    perror("Send Error");
+    return 1;
+  }
+
+  return 0;
 }
+
+int recv_client_input(int clientfd) {
+  char buffer[256] = {0};
+  if (recv(clientfd, buffer, 1, 0) == 0) {
+    printf("exiting\n");
+    return -1;
+  }
+  printf("%s\n", buffer);
+  return 0;  
+}
+
+int register_player();
+
+int pipe_player();
 
 int main() {
   int pipefd = open("../pipe", O_RDONLY);
   int socketfd = socket(AF_INET, SOCK_STREAM, 0);
   struct sockaddr address = {AF_INET, htons(9999), 0};
-
-  int true = 1;
 
   int bind_err = bind(socketfd, &address, sizeof(address));
   if (bind_err == -1) {
@@ -44,21 +74,18 @@ int main() {
 
   struct pollfd fds[2] = {{pipefd, POLLIN, 0}, {clientfd, POLLIN, 0}};
   for (;;) {
-    char buffer[256] = {0};
     poll(fds, 2, 50000);
     if (fds[0].revents & POLLIN) {
-      char *game_state = read_pipe(pipefd);
-      long send_err = send(clientfd, game_state, 255, 0);
-      if (send_err == -1) {
-        perror("Send Error");
+      int gs_err = send_game_state(pipefd, clientfd);
+      if (gs_err == -1) {
+        return -1;
       }
 
     } else if (fds[1].revents & POLLIN) {
-      if (recv(clientfd, buffer, 1, 0) == 0) {
-        printf("exiting\n");
-        return 0;
+      int exit = recv_client_input(clientfd); 
+      if (exit) {
+        return 0; 
       }
-      printf("%s\n", buffer);
     }
   }
 
